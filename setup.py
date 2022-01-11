09eff30f975017976d813ea88ab2814e236d85ed
sakaguchi.syotaro <53217796+guchitarou@users.noreply.github.com>
2022-01-11 16:29:12 +0900

    v3_2022

---
+++ setup.py
@@ -0,0 +1,25 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="DaTGen",
+    version="0.1.0",
+    license="syo",
+    description="demo code",
+    author="guchitarou",
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=_requires_from_file('requirements.txt'),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"]
+)
